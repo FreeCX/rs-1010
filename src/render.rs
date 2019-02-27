154911f99a899e1290e4f8ba75fbac5ca667047d
Alexey Golubev <dr.freecx@gmail.com>
2019-02-27 19:25:31 +0400

    code improvements
    - add own polygon function
    - add all figures
    - upd BasketSystem
    - upd & restruct code base
    - remove pallete & gfx

--- src/render.rs
+++ src/render.rs
@@ -1,30 +1,55 @@
 use crate::extra::Coord;
-use palette::{LinSrgb, Shade};
-use sdl2::gfx::primitives::DrawRenderer;
 use sdl2::pixels::Color;
+use sdl2::rect::Point;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
 
-fn light_up(c: Color, l: f32) -> Color {
-    let color = LinSrgb::new(c.r as f32 / 255.0, c.g as f32 / 255.0, c.b as f32 / 255.0).lighten(l);
-    let to_u8 = |x: f32| (x * 255.0).round() as u8;
-    Color::RGB(to_u8(color.red), to_u8(color.green), to_u8(color.blue))
+// very simple & unstable polygon drawing
+pub fn polygon(canvas: &mut Canvas<Window>, v: &mut [(i16, i16)], c: Color) -> Result<(), String> {
+    v.sort_by(|a, b| a.1.cmp(&b.1));
+    let (min_y, max_y) = (v[0].1, v[v.len() - 1].1);
+    let (mut min_x, mut max_x) = (0, 0);
+    let mut lines = Vec::new();
+    for y in min_y..=max_y {
+        let mut c: Vec<_> = v.iter().filter(|x| x.1 == y).collect();
+        if c.len() > 0 {
+            c.sort_by(|a, b| a.0.cmp(&b.0));
+            min_x = c[0].0;
+            max_x = c[c.len() - 1].0;
+            for (a, b) in c.iter().zip(c.iter().skip(1)) {
+                lines.push(Point::new(a.0 as i32, y as i32));
+                lines.push(Point::new(b.0 as i32, y as i32));
+            }
+        } else {
+            lines.push(Point::new(min_x as i32, y as i32));
+            lines.push(Point::new(max_x as i32, y as i32));
+        }
+    }
+    let last_color = canvas.draw_color();
+    canvas.set_draw_color(c);
+    for index in (0..lines.len() - 1).step_by(2) {
+        canvas.draw_line(lines[index + 0], lines[index + 1])?;
+    }
+    canvas.set_draw_color(last_color);
+    Ok(())
 }
 
+// TODO: optimize code for using canvas.draw_lines in polygon
 pub fn fill_rounded_rect(canvas: &mut Canvas<Window>, c1: Coord, c2: Coord, r: i16, c: Color) -> Result<(), String> {
-    let mut tmp: Vec<Vec<(i16, i16)>> = vec![Vec::new(); 8];
+    // prepare points for polygon
+    let mut tmp: Vec<(i16, i16)> = Vec::new();
     let (mut x, mut y) = (r - 1, 0);
     let (mut dx, mut dy) = (1, 1);
     let mut err = dx - (r << 1);
     while x >= y {
-        tmp[0].push((x - r, y - r)); // 8
-        tmp[1].push((y - r, x - r)); // 7
-        tmp[2].push((r - y, x - r)); // 6
-        tmp[3].push((r - x, y - r)); // 5
-        tmp[4].push((r - x, r - y)); // 4
-        tmp[5].push((r - y, r - x)); // 3
-        tmp[6].push((y - r, r - x)); // 2
-        tmp[7].push((x - r, r - y)); // 1
+        tmp.push((c2.x + x - r, c1.y + r - y)); // 1
+        tmp.push((c2.x + y - r, c1.y + r - x)); // 2
+        tmp.push((c1.x + r - y, c1.y + r - x)); // 3
+        tmp.push((c1.x + r - x, c1.y + r - y)); // 4
+        tmp.push((c1.x + r - x, c2.y + y - r)); // 5
+        tmp.push((c1.x + r - y, c2.y + x - r)); // 6
+        tmp.push((c2.x + y - r, c2.y + x - r)); // 7
+        tmp.push((c2.x + x - r, c2.y + y - r)); // 8
         if err <= 0 {
             y += 1;
             err += dy;
@@ -36,22 +61,7 @@ pub fn fill_rounded_rect(canvas: &mut Canvas<Window>, c1: Coord, c2: Coord, r: i
             err += dx - (r << 1);
         }
     }
-    let data = [(c2.x, c2.y), (c1.x, c2.y), (c1.x, c1.y), (c2.x, c1.y)];
-    let (mut vx, mut vy) = (Vec::new(), Vec::new());
-    for (index, (x, y)) in data.iter().enumerate() {
-        for (tx, ty) in &tmp[index * 2 + 0] {
-            vx.push(x + tx);
-            vy.push(y + ty);
-        }
-        for (tx, ty) in tmp[index * 2 + 1].iter().rev() {
-            vx.push(x + tx);
-            vy.push(y + ty);
-        }
-    }
-    canvas.filled_polygon(&vx, &vy, c)?;
-    let nc = light_up(c, 0.02);
-    for (x, y) in vx.into_iter().zip(vy) {
-        canvas.pixel(x, y, nc)?;
-    }
+    // draw
+    polygon(canvas, &mut tmp, c)?;
     Ok(())
 }
