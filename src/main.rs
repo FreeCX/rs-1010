412efe864f0f6295d512df7a9439a1430627c5dc
Alexey Golubev <dr.freecx@gmail.com>
2019-03-05 12:39:16 +0400

    add panic handling & fix game timer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![windows_subsystem = "windows"]
 #![allow(unused_assignments)]
+extern crate backtrace;
 extern crate sdl2;
 extern crate tini;
 use sdl2::event::Event;
@@ -7,6 +8,8 @@ use sdl2::keyboard::Keycode;
 use sdl2::mouse::MouseButton;
 use sdl2::pixels::Color;
 use std::time::SystemTime;
+use std::panic;
+use std::{fs::File, io::Write};
 use tini::Ini;
 
 #[macro_use]
@@ -15,7 +18,61 @@ mod game;
 mod random;
 mod render;
 
+// handle panic and write crash repot to file
+fn panic_handler(panic_info: &panic::PanicInfo) {
+    let mut buffer = String::new();
+    buffer.push_str(&format!(
+        "The application had a problem and crashed.\n\
+         To help us diagnose the problem you can send us a crash report.\n\n\
+         Authors: {}\n\n\
+         We take privacy seriously, and do not perform any automated error collection.\n\
+         In order to improve the software, we rely on people to submit reports.\n\n\
+         Thank you!\n\n\
+         --- crash report start ---\n\
+         name: {}\n\
+         version: {}\n\n",
+        env!("CARGO_PKG_AUTHORS"),
+        env!("CARGO_PKG_NAME"),
+        env!("CARGO_PKG_VERSION")
+    ));
+    match panic_info.location() {
+        Some(location) => {
+            let info = format!("panic occurred in file '{}' at line {}\n", location.file(), location.line());
+            buffer.push_str(&info);
+        }
+        None => buffer.push_str("panic occurred but can't get location information...\n"),
+    }
+    buffer.push_str("stack backtrace:\n");
+    let mut index = 0;
+    backtrace::trace(|frame| {
+        let ip = frame.ip();
+        let symbol_address = frame.symbol_address();
+        backtrace::resolve(ip, |symbol| {
+            if let Some(name) = symbol.name() {
+                let symbol_info = format!("\t{}: {} @ {:?}\n", index, name, symbol_address);
+                buffer.push_str(&symbol_info);
+                index += 1;
+            }
+            match (symbol.filename(), symbol.lineno()) {
+                (Some(filename), Some(line)) => {
+                    let file_info = format!("\t\t\tat {}:{}\n", filename.display(), line);
+                    buffer.push_str(&file_info);
+                }
+                _ => {}
+            }
+        });
+        true
+    });
+    buffer.push_str("--- crash report end ---");
+    File::create("crash.log")
+        .and_then(|mut file| write!(file, "{}", buffer))
+        .unwrap_or_else(|_| println!("{}", buffer));
+}
+
 fn main() {
+    // handle panics
+    panic::set_hook(Box::new(panic_handler));
+
     // default config
     const DEFAULT_CONFIG: &'static str = r#"
         [config]
