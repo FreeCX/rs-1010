60a1ce428775fb10182b41b249aa843a8b4ec206
Alexey Golubev <dr.freecx@gmail.com>
2019-03-08 18:00:57 +0400

    add score table & build info

---
+++ build.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+use std::process::Command;
+use std::{fs::File, io::Write};
+
+fn execute(cmd: &str) -> String {
+    match Command::new(cmd).arg("-vV").output() {
+        Ok(value) => match String::from_utf8(value.stdout) {
+            Ok(value) => value,
+            Err(_) => "unknown".to_string()
+        },
+        Err(_) => "unknown".to_string()
+    }
+}
+
+fn parse(s: String) -> HashMap<String, String> {
+    let mut res = HashMap::new();
+    for line in s.lines() {
+        let block: Vec<&str> = line.split(":").collect();
+        if block.len() == 1 {
+            res.insert("header".to_string(), block[0].trim().to_string());
+        } else {
+            res.insert(block[0].to_string(), block[1].trim().to_string());
+        }
+    }
+    res
+}
+
+fn main() {
+    let mut source_code = "#![allow(dead_code)]\n".to_string();
+    for (prefix, executable) in [("RUST", "rustc"), ("CARGO", "cargo")].iter() {
+        let iterator = parse(execute(executable));
+        for (k, v) in iterator {
+            let key = k.to_uppercase().replace("-", "_").replace(" ", "_");
+            let fmt_str = format!("pub static {}_{}: &'static str = \"{}\";\n", prefix, key, v);
+            source_code.push_str(&fmt_str);
+        }
+    }
+    File::create("src/build.rs")
+        .and_then(|mut file| write!(file, "{}", source_code))
+        .unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,21 @@ extern crate backtrace;
 extern crate sdl2;
 extern crate tini;
 use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
+use sdl2::keyboard::{Keycode, Scancode};
 use sdl2::mouse::MouseButton;
 use sdl2::pixels::Color;
-use std::time::SystemTime;
 use std::panic;
+use std::time::SystemTime;
 use std::{fs::File, io::Write};
 use tini::Ini;
 
 #[macro_use]
 mod extra;
+mod build;
 mod game;
 mod random;
 mod render;
+mod score;
 
 // handle panic and write crash repot to file
 fn panic_handler(panic_info: &panic::PanicInfo) {
@@ -30,10 +32,17 @@ fn panic_handler(panic_info: &panic::PanicInfo) {
          Thank you!\n\n\
          --- crash report start ---\n\
          name: {}\n\
-         version: {}\n\n",
+         version: {}\n\n\
+         compiler: {}\n\
+         package manager: {}\n\
+         host: {}\n\n\
+         ",
         env!("CARGO_PKG_AUTHORS"),
         env!("CARGO_PKG_NAME"),
-        env!("CARGO_PKG_VERSION")
+        env!("CARGO_PKG_VERSION"),
+        build::RUST_HEADER,
+        build::CARGO_HEADER,
+        build::RUST_HOST,
     ));
     match panic_info.location() {
         Some(location) => {
@@ -75,18 +84,19 @@ fn main() {
 
     // default config
     const DEFAULT_CONFIG: &'static str = r#"
-        [config]
-        magnetization = true
-        blend = true
-        alpha = 150
-        fps = 30
-        [score]
-        value = 0
-        time = 00:00:00
-    "#;
+    [config]
+    magnetization = true
+    blend = true
+    alpha = 150
+    fps = 30
+    [score]
+    users =
+    scores =
+    times ="#;
     // resource & config
     const FONT_FILE: &'static str = "./resources/FiraMono-Regular.ttf";
     const CONFIG_FILE: &'static str = "./resources/config.ini";
+    const GAMESCORE_COUNT: usize = 5;
     // game title
     const GT: &'static str = "1010";
     const MILLISECOND: u32 = 1000;
@@ -112,6 +122,7 @@ fn main() {
     const W_WIDTH: u32 = FIELD_WIDTH + BASKET_LEN;
     const W_HEIGHT: u32 = FIELD_WIDTH;
     // font consts
+    const FONT_MIN_SIZE: u16 = 12;
     const FONT_DEF_SIZE: u16 = 18;
     const FONT_BIG_SIZE: u16 = 48;
     const FONT_HEIGHT: i16 = FONT_DEF_SIZE as i16 + 2;
@@ -123,6 +134,7 @@ fn main() {
     };
     let magnetization = config.get("config", "magnetization").unwrap_or(true);
     let alpha_value = config.get("config", "alpha").unwrap_or(150);
+    let mut score_table = score::ScoreTable::from_config(&config);
 
     // available game figures
     let figures = vec![
@@ -166,18 +178,6 @@ fn main() {
     let mut mouse_pos = coord!(0, 0);
     let mut figure_pos = coord!(0, 0);
 
-    // game scores
-    let mut highscore = config.get("score", "value").unwrap_or(0);
-    let mut score: u32 = 0;
-    // game over params
-    let mut gameover_flag = false;
-    let gameover_pos = coord!(100, (W_HEIGHT as i16 - FONT_BIG_SIZE as i16 - 5) / 2);
-    let bp3 = gameover_pos - coord!(10, 0);
-    let bp4 = gameover_pos + coord!(269, FONT_BIG_SIZE as i16 + 8);
-    let border_size = coord!(8, 8);
-    let bp1 = bp3 - border_size;
-    let bp2 = bp4 + border_size;
-
     // SDL2
     let sdl_context = sdl2::init().expect("Can't init sdl2 context");
     let video_subsystem = sdl_context.video().expect("Can't create video subsystem");
@@ -189,6 +189,19 @@ fn main() {
     // TODO: rewrite to Font struct
     let font = msg!(ttf_context.load_font(FONT_FILE, FONT_DEF_SIZE); canvas.window(), GT);
     let font_big = msg!(ttf_context.load_font(FONT_FILE, FONT_BIG_SIZE); canvas.window(), GT);
+    let font_min = msg!(ttf_context.load_font(FONT_FILE, FONT_MIN_SIZE); canvas.window(), GT);
+
+    // game scores
+    let mut highscore = score_table.get_highscore();
+    let mut score: u32 = 0;
+    // game over params
+    let mut gameover_flag = false;
+    let mut user_name = String::new();
+    let game_over = "GAME OVER";
+    // rendering params
+    let (fsx, fsy) = font_big.size_of(game_over).unwrap();
+    let mut name_input_flag = false;
+    let border = 6_i16;
 
     // turn on alpha channel
     if config.get("config", "blend").unwrap_or(true) {
@@ -228,11 +241,44 @@ fn main() {
         msg!(field.render(&mut canvas, field_bg_color, ROUND_RADIUS); canvas.window(), GT);
         msg!(basket.render(&mut canvas, field_bg_color, ROUND_RADIUS); canvas.window(), GT);
 
-        if gameover_flag {
-            // render gameover message
-            msg!(render::fill_rounded_rect(&mut canvas, bp1, bp2, BIG_ROUND_RADIUS, border_color); canvas.window(), GT);
-            msg!(render::fill_rounded_rect(&mut canvas, bp3, bp4, BIG_ROUND_RADIUS, bg_color); canvas.window(), GT);
-            msg!(render::font(&mut canvas, &font_big, gameover_pos, font_color, "GAME OVER"); canvas.window(), GT);
+        if gameover_flag && !name_input_flag {
+            let mut scores = Vec::new();
+            let mut ss = coord!(0, 0);
+            let name_size = 14;
+            for (index, score::Score { name, score, time }) in score_table.iter().take(GAMESCORE_COUNT).enumerate() {
+                let name =
+                    if name.len() > name_size { format!("{}...", &name[..name_size - 3]) } else { format!("{}", name) };
+                let score = format!("{}. {: <4$} {:08} ({})", index + 1, name, score, time, name_size);
+                let (ssx, ssy) = font_min.size_of(&score).unwrap();
+                ss.y += ssy as i16;
+                ss.x = ss.x.max(ssx as i16);
+                scores.push(score);
+            }
+            let fp1 = coord!((W_WIDTH as i16 - fsx as i16) >> 1, (W_HEIGHT as i16 - fsy as i16 - ss.y) >> 1);
+            let p1 = fp1 - 2 * border;
+            let p2 = fp1 + coord!(fsx as i16, ss.y + fsy as i16 - border) + 2 * border;
+            let p3 = p1 + border;
+            let p4 = p2 - border;
+            msg!(render::fill_rounded_rect(&mut canvas, p1, p2, BIG_ROUND_RADIUS, border_color); canvas.window(), GT);
+            msg!(render::fill_rounded_rect(&mut canvas, p3, p4, BIG_ROUND_RADIUS, bg_color); canvas.window(), GT);
+            msg!(render::font(&mut canvas, &font_big, fp1, font_color, game_over); canvas.window(), GT);
+            for (index, text) in scores.iter().enumerate() {
+                let fp2 = fp1 + coord!(0, fsy as i16 + index as i16 * (ss.y / scores.len() as i16)) - coord!(0, border);
+                msg!(render::font(&mut canvas, &font_min, fp2, font_color, text); canvas.window(), GT);
+            }
+        } else if gameover_flag && name_input_flag {
+            let ssy = (2 * FONT_MIN_SIZE) as i16;
+            let fp1 = coord!((W_WIDTH as i16 - fsx as i16) >> 1, (W_HEIGHT as i16 - fsy as i16 - ssy) >> 1);
+            let p1 = fp1 - 2 * border;
+            let p2 = fp1 + coord!(fsx as i16, ssy + fsy as i16 - border) + 2 * border;
+            let p3 = p1 + border;
+            let p4 = p2 - border;
+            let fp2 = fp1 + coord!(0, fsy as i16 - border);
+            let input_name = format!("ваше имя: {}", user_name);
+            msg!(render::fill_rounded_rect(&mut canvas, p1, p2, BIG_ROUND_RADIUS, border_color); canvas.window(), GT);
+            msg!(render::fill_rounded_rect(&mut canvas, p3, p4, BIG_ROUND_RADIUS, bg_color); canvas.window(), GT);
+            msg!(render::font(&mut canvas, &font_big, fp1, font_color, game_over); canvas.window(), GT);
+            msg!(render::font(&mut canvas, &font, fp2, font_color, &input_name); canvas.window(), GT);
         } else {
             // or count game timer
             game_stop = game_start.elapsed();
@@ -256,18 +302,41 @@ fn main() {
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. } | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => break 'running,
+                Event::KeyDown { scancode: Some(value), .. } => {
+                    if name_input_flag {
+                        match value {
+                            Scancode::Return => {
+                                score_table.push(user_name.clone(), score, extra::as_time_str(&game_stop));
+                                user_name.clear();
+                                name_input_flag = false;
+                            }
+                            Scancode::Backspace => {
+                                user_name.pop();
+                            }
+                            _ => ()
+                        }
+                        let s_value = value.name();
+                        // sdl2 scancode filter hack
+                        if s_value.len() == 1 {
+                            user_name.push_str(value.name());
+                        }
+                    }
+                }
                 Event::MouseMotion { x, y, .. } => {
                     mouse_pos.x = x as i16;
                     mouse_pos.y = y as i16;
                 }
                 Event::MouseButtonDown { mouse_btn: MouseButton::Left, x, y, .. } => {
-                    if gameover_flag {
+                    if gameover_flag && !name_input_flag {
                         // restart game
                         gameover_flag = false;
                         basket.fill(&figures);
                         field.clear();
                         score = 0;
                         game_start = SystemTime::now();
+                        continue;
+                    }
+                    if gameover_flag {
                         continue;
                     }
                     // figure set | return
@@ -300,8 +369,10 @@ fn main() {
         highscore = highscore.max(score);
         // check gameover
         if !field.can_set(basket.figures()) && current_figure == None {
+            if !gameover_flag && !name_input_flag {
+                name_input_flag = true;
+            }
             gameover_flag = true;
-            current_figure = None;
         }
 
         // fps counter
@@ -316,11 +387,8 @@ fn main() {
         }
     }
 
-    if config.get("score", "value") < Some(highscore) {
-        // update game highscore
-        msg!(config.section("score")
-                       .item("value", &format!("{}", highscore))
-                       .item("time", &extra::as_time_str(&game_stop))
-                       .to_file(CONFIG_FILE); canvas.window(), GT);
-    }
+    // add game score when game closed
+    score_table.push("<game-close>".to_string(), score, extra::as_time_str(&game_stop));
+    // update highscore results
+    msg!(score_table.to_config(GAMESCORE_COUNT, config).to_file(CONFIG_FILE); canvas.window(), GT);
 }
